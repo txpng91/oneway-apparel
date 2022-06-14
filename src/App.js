b1f5e567f005f4632fe05b0fa7c85c92b849e852
txpng91 <petengarcia@gmail.com>
2022-06-14 00:29:18 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import NavBar from './components/NavBar';
+import Main from './Pages/Main';
+import About from './Pages/About';
+import Products from './Pages/Products';
+import Product from './Pages/Product';
+import Login from './Pages/Login';
+import Cart from './Pages/Cart';
+import Checkout from './Pages/Checkout';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Main />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/products/:id' element={<Product />} />
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/checkout' element={<Checkout />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+      </Router>
+    </>
   );
 }
 
